2108e4707eeae119296cba6aa872f262211585db
alex-muliande <amuliande@gmail.com>
2019-09-03 21:41:34 +0300

    Add configarations .

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 from flask import render_template
 from app import app
 
-#Views
+# Views
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
+
     '''
-    View movie page function that returns the index page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    message = 'Hello Alex'
-    return render_template('index.html',message = message)
+    return render_template('movie.html',id = movie_id)
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - welcome to the best movie Review website Online'
+    return render_template('insex.html', title = title) 
