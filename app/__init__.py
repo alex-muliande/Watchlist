0bf21bff2de159dceaed0489898b9ba1a0827d63
alex-muliande <amuliande@gmail.com>
2019-09-03 15:56:06 +0300

    new files ,installed flask, inintialized git , created files.

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+# Inintializing appplication
+app = Flask(__name__)
+
+from app import views
---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+#Views
+@app.route('/movie/movie_id')
+def movie(movie_id):
+    '''
+    View movie page function that returns the index page and its data
+    '''
+    message = 'Hello Alex'
+    return render_template('index.html',message = message)
