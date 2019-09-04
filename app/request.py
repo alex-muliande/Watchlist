4a28d5b641e6e974ed1a60e3221b73c0344dc74a
alex-muliande <amuliande@gmail.com>
2019-09-04 08:02:37 +0300

    Added get json request function .

--- app/request.py
+++ app/request.py
@@ -6,3 +6,10 @@ Movie = movie.Movie
 
 # Geting api key
 api_key = app.config['MOVIE_API_KEY']
+base_url = app.config["MOVIE_API_BASE_URL"]
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
