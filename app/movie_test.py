58783a0b513fedfd1a752d4495a399c69f5dd519
alex-muliande <amuliande@gmail.com>
2019-09-04 06:28:19 +0300

    Added request.py content .

--- app/movie_test.py
+++ app/movie_test.py
@@ -11,3 +11,11 @@ class MovieTest(unittest.TestCase):
         '''
         set up method that will run before every Test
         '''
+        self.new_movie = Movie(1234,'Python must be crazy', 'A thrilling new python series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie,movie))
+
+
+if__name__=='__main__'
+    unittest.main()
--- app/request.py
+++ app/request.py
@@ -1,4 +1,8 @@
 from app import app
+import urllib.request,json
+from .models import movie 
+
+Movie = movie.Movie
 
 # Geting api key
 api_key = app.config['MOVIE_API_KEY']
