8c78900991e1ed19d3fbbd4f1579bd4118eb9af0
alex-muliande <amuliande@gmail.com>
2019-09-05 16:43:38 +0300

    Add new file called review.py  .

---
+++ app/models/review.py
@@ -0,0 +1,15 @@
+class Review:
+
+    all_reviews = []
+
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+
+        return response
