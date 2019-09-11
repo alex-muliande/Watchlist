7f98ca2577a4552c2e3cf3464a541ceb215a333f
alex-muliande <amuliande@gmail.com>
2019-09-11 22:55:51 +0300

    changes made on the model.py file from import database.

--- app/__init__.py
+++ app/__init__.py
@@ -12,14 +12,11 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    # ....
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-
-    from .requests import configure_request
-    configure_request(app)
+    db.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
