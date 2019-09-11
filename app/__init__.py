1371dfed86a948aad94cdc9987350db7af30529b
alex-muliande <amuliande@gmail.com>
2019-09-11 18:48:49 +0300

    Create a config inside config.py, import SQLalchemy and create a database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 bootstrap = Bootstrap()
 
